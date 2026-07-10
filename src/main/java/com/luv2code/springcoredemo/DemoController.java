a98e2589974d3468744b8e5f20adfe76bc45d124
Rajesh <rajesh752791@gmail.com>
2026-07-10 11:26:19 +0530

    final state

--- src/main/java/com/luv2code/springcoredemo/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,12 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("swimCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
+    // We only need ONE mapping for our URL!
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
